0214a23e9c1069d8f2d52571c600d3227cba631d
psmattu <mattu.parteek@gmail.com>
2023-11-08 16:11:06 -0800

    Cleaning files and getting ready for project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import './App.css';
+import React from "react";
 
 const App = () => {
-  const name = 'Parteek';
-  const isNameShowing = false;
-  return (
-    <div className="App">
-      <h1>Hello, {isNameShowing ? name : 'someone'}!</h1>
-    </div>
-  );
+    return(
+        <h1>App</h1>
+    );
 }
 
 export default App;
