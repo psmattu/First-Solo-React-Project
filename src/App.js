5aeb0d03712931168ac6b6742d987865ce024454
psmattu <mattu.parteek@gmail.com>
2023-11-16 16:22:29 -0800

    Working application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
-import React from "react";
+import {useState,useEffect} from "react";
+
+import MovieCard from "./MovieCard";
+
+import './App.css';
+import searchIcon from './search.svg';
+
+// Use API_URL component to gather movie data.
+const API_URL = 'http://www.omdbapi.com?apikey=6e7cd360'
+
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Batman')
+    }, []);
+
     return(
-        <h1>App</h1>
+        <div className="app">
+            <h1>PSM Movies</h1>
+
+            <div className="search">
+                <input
+                placeholder="Search for movies"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)} // e = event
+                />
+
+                <img
+                src={searchIcon}
+                alt="search"
+                onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                                <MovieCard movie={movie} />
+                            ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )}
+
+    
+        </div>
     );
 }
 
